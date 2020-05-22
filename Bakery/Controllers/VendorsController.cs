002b572cc32696871482fd24b4928512fd36ace6
nkhakwan <post_khan@yahoo.com>
2020-05-22 16:32:54 -0700

    changes to conrtoller file

--- Bakery/Controllers/VendorsController.cs
+++ Bakery/Controllers/VendorsController.cs
@@ -54,6 +54,18 @@ namespace Bakery.Controllers
       return View(model);
     }
 
+    [HttpGet("/vendor/{vendorId}/orders/{orderId}")]
+    public ActionResult OrderShow(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+
     [HttpGet("/order/new")]
     public ActionResult OrderNew()
     {
